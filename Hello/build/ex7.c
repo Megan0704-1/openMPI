d5aebf8434887054326b82228e9907a7047c8a0f
megakuo <megan20010704@gmail.com>
2024-01-09 11:12:29 +0800

    Hello

---
+++ Hello/build/ex7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+    int distance = 100;
+    float power = 2.345f;
+    double super_power = 5678.233;
+    char initial = 'A';
+    char first_name[] = "Megan";
+    char last_name[] = "Kuo";
+
+    printf("You are %d miles away.\n", distance);
+    printf("My name is %s %s", first_name, last_name);
+}
